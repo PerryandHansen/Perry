5584f7bd394c3743db64239055ea439d2ea2af32
PerryandHansen <47538813+PerryandHansen@users.noreply.github.com>
2019-03-05 13:05:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -142,59 +142,47 @@ open_and_close_claw (1024);//open claw
 void drive_forward(int distance)
 {
     cmpc(0);
-    While (gmpc(0)<(distance*(1400/17))) //distance*(ticks/cm)
-     {
-         motor(0,50)
-         motor(3,50)
-      }
-    ao();
-}
-void turn_left(int degrees)
-{ cmpc(0);
-while (abs(gmpc(0)<(abs(degrees*(1000/90)))))
+    while (gmpc(0)<((distance*1400)/17)) //distance times ticks per cm
     {
-        mav(0,1000);
-        mav(3,-1000);
-        msleep(10);
+         mav(0,900);
+         mav(3,800);       
     }
-    cmpc(0);
     ao();
-  }
-void turn_right(int degrees)
-{ cmpc(3);
-while (abs(gmpc(3)<(abs(degrees*(1000/98)))))
-    {
-        mav(0,-1000);
-        mav(3,1000);
-        msleep(10);
-    }
- cmpc(3);
- ao();
-  }
-void drive_back(int distance)  //input in cm
-{
     cmpc(0);
-    while(gmpc(0)<(distance*(-1400/17)))
-    {
-         motor(0,-50);
-         motor(3,-50);
-     }
-    ao();
-}
- void open_and_close_claw (int position)
-{
-    enable_servos();
-    set_servo_position(2, position);
-       msleep(1000);
-   
 }
 
-void lift_and_lower_claw(int position)
+void turn_right(int degrees)
+{ 
+    cmpc(3);
+	while (abs(gmpc(3)<(abs(degrees*10))))
+    {
+        mav(0,-300);
+        mav(3,300);
+        msleep(10);
+    }
+    cmpc(3);
+    ao();
+}
+
+void turn_left(int degrees)
+{
+    cmpc(0);
+    cmpc(3);
+	while (abs(gmpc(0)<(abs(degrees*10))))
+    {
+        mav(0,300);
+        mav(3,-200);
+        msleep(10);
+    }
+}
+
+void lift_and_lower_claw(int position)//1730 HIGH 450 low
 {
     enable_servos();
     set_servo_position(0, position);
-       msleep(1000);
+    msleep(1000);
 }
+
 
 void slow_servo_lift(int position)
 {
@@ -202,28 +190,135 @@ void slow_servo_lift(int position)
     int x = 0;
     while(x<position)
     {
-    set_servo_position(0, x);
-       msleep(50);
-      x+=25;
+    	set_servo_position(0, x);
+        msleep(50);
+        x+=25;
     }
 }
 
-void rotate_claw(int position)
+void slow_servo_lower(int position)
+{
+    enable_servos();
+    int x = 0;
+    while(x<position)
+    {
+    	set_servo_position(0, x);
+        msleep(50);
+        x-=25;
+    }
+}
+
+void open_and_close_claw (int position)// limit: 1500closed 600open
+{
+    enable_servos();
+    set_servo_position(2, position);
+    msleep(1000);   
+}
+
+void slow_servo_close(int position)
+{
+    enable_servos();
+    int x = 0;
+    while(x<position)
+    {
+    	set_servo_position(2, x);
+        msleep(50);
+        x+=25;
+    }
+}
+
+void slow_servo_open(int position)
+{
+    enable_servos();
+    int x = 0;
+    while(x<position)
+    {
+    	set_servo_position(2, x);
+        msleep(50);
+        x-=15;
+    }
+}
+
+void drive_back (int distance)
+{
+    cmpc(0);
+    while (abs(gmpc(0))<(abs(distance*(1400/17)))) //distance times ticks per cm 
+    {
+         mav(0,-500);
+         mav(3,-500);
+    }
+    ao();
+    cmpc(0);
+}
+
+void rotate_claw(int position)//limit: 1800 left 600 right 
 {
     enable_servos();
     set_servo_position(1, position);
-       msleep(1000);
+    msleep(1000);
    
 }
+
+void slow_rotate_left(int position) //right is less, left is more
+{
+    enable_servos();
+    int x = 0;
+    while(x<position)
+    {
+    	set_servo_position(1, x);
+        msleep(50);
+        x+=25;
+    }
+}
+
+void slow_rotate_right(int position)
+{
+    enable_servos();
+    int x = 0;
+    while(x<position)
+    {
+    	set_servo_position(1, x);
+        msleep(50);
+        x-=25;
+    }
+}
+
+
 void curve_drive(int distance)
 {
     cmpc(0);
     while(gmpc(0)<(distance*(-1400/17)))
     {
-	mav(0, 750);
-	mav(3,500);
+		mav(0,750);
+		mav(3,500);
     }
     ao();
+}
+
+void back_bump()
+{ 
+    while (digital(0)==0 || digital(8)==0)
+    {
+       
+        if (digital(8)==1)
+        {  
+            mav(0,1000);
+            mav(3,0);
+        }
+        
+        if (digital(0)==1)       
+        { 
+            mav(0,0);
+            mav(3,1000);
+        }
+    
+        else
+        {
+              mav(0,900);
+              mav(3,900);
+        }
+    }
+    ao();  
 }
 
 
